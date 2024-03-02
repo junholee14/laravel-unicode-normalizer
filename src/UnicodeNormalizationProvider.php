ed60597b11bc5c043abd971b28a1119ab092db30
junholee14 <junho2545@naver.com>
2024-03-02 23:47:32 +0900

    chore: Move UnicodeNormalizationProvider.php path

---
+++ src/UnicodeNormalizationProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Junholee14\LaravelUnicodeNormalizer;
+
+use Illuminate\Support\ServiceProvider;
+
+final class UnicodeNormalizationProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                    __DIR__ . '/resources/lang' => resource_path('lang/vendor/laravelUnicodeNormalizer')
+                ], 'lang');
+            $this->publishes([
+                __DIR__ . '/config/unicode-normalizer.php' => config_path('unicode-normalization.php')
+            ], 'config');
+        }
+
+        $this->loadTranslationsFrom(__DIR__ . '/resources/lang', 'laravelUnicodeNormalizer');
+        $this->mergeConfigFrom(__DIR__ . '/config/unicode-normalizer.php', 'unicode-normalizer');
+    }
+}
--- tests/PackageTestCase.php
+++ tests/PackageTestCase.php
@@ -2,7 +2,7 @@
 
 namespace Junholee14\LaravelUnicodeNormalizer\Tests;
 
-use Junholee14\LaravelUnicodeNormalizer\Providers\UnicodeNormalizationProvider;
+use Junholee14\LaravelUnicodeNormalizer\UnicodeNormalizationProvider;
 use Orchestra\Testbench\TestCase;
 
 class PackageTestCase extends TestCase
